dc1c4f30fc47d02c0f9a28a9bbc0245825250eb1
rpikuza <rpikuza@instartlogic.com>
2018-08-06 19:36:15 +0300

    Fix identations

--- AuthTest.py
+++ AuthTest.py
@@ -4,30 +4,32 @@ import json
 
 class AuthTest:
     def __init__(self, url):
-    self.base_url = url
-    self.endpoints = {
-        "login": "/api/auth/login"
-    }
-    self.login = "hello@world.com"
-    self.password = "12345678"
+        self.base_url = url
+        self.endpoints = {
+            "login": "/api/auth/login"
+        }
+        self.login = "hello@world.com"
+        self.password = "12345678"
         
-        def login_fail():
-            login_url = self.base_url + self.endpoints['login']
-            data = json.dumps({
-                              'login': self.login,
-                              'password': self.password
-                              })
-                expected_data = {
-"success": false,
-    "errors": {
-        "email": [""],
-        "password": [ ""],
-        "message": "The password and email you entered don't match. If you forgot your password, use \"Forgot Password\""
-    }
-    }
-    r = requests.post(login_url, data=data)
-    actual_data = json.loads(r.text)
-    assert actual_data == expected_data, "Login fail error expected. Got %s" % actual_data
+    def login_fail():
+        login_url = self.base_url + self.endpoints['login']
+        data = json.dumps(
+            {
+                'login': self.login,
+                'password': self.password
+            }
+        )
+        expected_data = {
+            "success": false,
+            "errors": {
+                "email": [""],
+                "password": [ ""],
+                "message": "The password and email you entered don't match. If you forgot your password, use \"Forgot Password\""
+            }
+        }
+        r = requests.post(login_url, data=data)
+        actual_data = json.loads(r.text)
+        assert actual_data == expected_data, "Login fail error expected. Got %s" % actual_data
 
 def runTests():
     auth_test = new AuthTest(url='http://instatestvx.me')
